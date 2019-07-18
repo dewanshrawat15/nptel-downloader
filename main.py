9b2ffbf75c1a18feae081ef920b3ede8505edc8b
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-07-18 22:43:44 +0530

    Updating main.py
    Almost done.

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
-from html.parser import HTMLParser
-import sys
-import urllib3
-links = []
+import requests
+from urllib.request import urlopen, urljoin
+from bs4 import BeautifulSoup
+import os
+from os import system, name
+import urllib.parse as urlparse
 
-class Parser(HTMLParser):
+def clear():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system("clear")
 
-	def handle_starttag(self, tag, attrs):
-		if tag != 'a':
-			return
-		attr = dict(attrs)
-		links.append(attr)
+class Downloader:
 
-def start():
-	if len(sys.argv) != 2:
-		print('Usage: {} URL'.format(sys.argv[0]))
-		return
-	url = sys.argv[1]
-	try:
-		f = urllib.request.urlopen(url)
-		html = f.read()
-		f.close()
-	except:
-		print('while fetching', url)
-		return
-	parser = MyHTMLParser()
-	parser.links = []
-	parser.feed(html)
-	for l in links:
-		print(l)
+	def download(self, link, filename):
+		r = requests.get(link, stream = True)
+		with open(filename, 'wb') as f:
+			for chunk in r.iter_content(chunk_size = 1024*1024):
+				if chunk:
+					f.write(chunk)
+		print(filename)
 
-start()
+	def scrape(self, url):
+		src = urlopen(url)
+		codebase = BeautifulSoup(src, 'html.parser')
+		y = codebase.findAll("a")
+		req = []
+		names = []
+		print("Format of videos ? (FLV, MP4 or 3GP): ")
+		x = input()
+		x = x.lower()
+		for i in y:
+			temp = i.get("href")
+			if x in temp:
+				a = urljoin(url, temp)
+				parsed = urlparse.urlparse(a)
+				name = urlparse.parse_qs(parsed.query)['subjectName']
+				for g in name:
+					names.append(g)
+				req.append(a)
+
+		for i in range(len(req)):
+			print("File Name => " + names[i])
+			print("Link => " + req[i])
+			self.download(req[i], names[i])
+
+clear()
+x = input("Enter URL address of download page: ")
+download = Downloader()
+download.scrape(x)
