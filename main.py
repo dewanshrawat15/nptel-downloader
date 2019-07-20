2bc2ded47ad916e4a71f10bcbcc8c01561839b4a
dewansh rawat <dewanshrawat15@gmail.com>
2019-07-20 17:06:01 +0530

    Adding flags
    Added two flags successfully, -u for downloading straight from url, -f for download video format

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sentry_sdk
+# import sentry_sdk
 from tqdm import tqdm
 import requests
 from urllib.request import urlopen, urljoin
@@ -7,6 +7,7 @@ import os
 from os import system, name
 import urllib.parse as urlparse
 import sys
+import argparse
 
 def clear():
     if name == 'nt':
@@ -36,15 +37,18 @@ class Downloader:
                         f.write(data)
             print(downloaded_file_name+" downloaded")
 
-    def scrape(self, url):
+    def scrape(self, url, vid_format):
         src = urlopen(url)
         codebase = BeautifulSoup(src, 'html.parser')
         y = codebase.findAll("a")
         req = []
         names = []
-        print("Format of videos ? (FLV, MP4 or 3GP): ")
-        x = input()
-        x = x.lower()
+        if vid_format is None:
+            print("Format of videos ? (FLV, MP4 or 3GP): ")
+            x = input()
+            x = x.lower()
+        else:
+            x = vid_format
         for i in y:
             temp = i.get("href")
             if x in temp:
@@ -60,8 +64,33 @@ class Downloader:
         for i in range(len(req)):
             self.download(req[i], names[i], x)
 
-clear()
-x = input("Enter URL address of download page: ")
-# Course to be downloaded https://nptel.ac.in/courses/nptel_download.php?subjectid=106102064
-download = Downloader()
-download.scrape(x)
+    def fetch_page(self, course_url, vid_format):
+        src = urlopen(course_url)
+        codebase = BeautifulSoup(src, 'html.parser')
+        links = codebase.findAll("a")
+        for i in links:
+            urlText = i.getText()
+            if urlText == "Download Videos & Transcripts":
+                downloadPageURL = urljoin(course_url, i.get("href"))
+                break
+        self.scrape(downloadPageURL, vid_format)
+
+def start():
+    clear()
+    parser = argparse.ArgumentParser(description='NPTEL Downloader. Download the videos of your favorite course on NPTEL. Just paste the web address of the course page, and start downloading those videos. ')
+    parser.add_argument("-u", "--url", help="Enter the course page url")
+    parser.add_argument("-f", "--format", help="Enter the format in which the videos have to be downloaded")
+    # parser.add_argument("-p", "--path", help="Enter the path where the videos have to be downloaded")g
+    args = parser.parse_args()
+    if not args.url:
+        x = input("Enter URL address of download page: ")
+        # Course to be downloaded https://nptel.ac.in/courses/nptel_download.php?subjectid=106102064
+    download = Downloader()
+    supported_formats = ['mp4', 'flv', '3gp']
+    if args.format and args.format.lower() in supported_formats:
+        download.fetch_page(args.url, args.format.lower())
+    else:
+        download.fetch_page(x, None)
+
+if __name__ == "__main__":
+    start()
